9613f036f4ddecba69d3367da86bf60e6dac644d
chuanjin <suchuanjin321@gmail.com>
2020-04-01 16:18:05 +0200

    add a function to read super large JSON file for multi-processing

--- code/data_loading.py
+++ code/data_loading.py
@@ -1,6 +1,8 @@
 import json
 import task
-import time_personalized 
+import time_personalized
+import ijson
+from numpy import ceil,inf
 
 
 def loadTasks(doc):
@@ -31,6 +33,75 @@ def loadTasks(doc):
             tasks[key + 1 - first].dependence = newdep
             del tasks[key]
     return tasks
+
+
+def tasksCount(doc):
+    '''
+    This fuction counts number of tasks in a large JSON file
+    '''
+    count = 0
+    with open(doc, 'r') as f:
+        parser = ijson.parse(f)
+        for prefix, event, _ in parser:
+            if event=='map_key' and prefix=='nodes':
+                count += 1
+    return count
+
+
+def loadTasksLargeJson(doc, Me=0, NbP=4, tasksNumber=None, deleteDependencies=True):
+    '''
+    :param doc: path of a proper large JSON file
+    :Me: rank of current process
+    :NbP: number of processes
+    :tasksNumber(optional): number of nodes in the JSON file
+    :deleteDependencies(optional): to delete dependencies that are smaller than the first node
+    :return: a Dictionnary with the tasks ID, time and dependancies
+    '''
+
+    # If isn't give tasksNumber. Note that give this parameter to save time.
+    if tasksNumber==None:
+        tasksNumber = tasksCount(doc)
+
+    nodeStart = ceil(tasksNumber/NbP) * Me
+    nodeEnd = ceil(tasksNumber/NbP) * (Me+1)
+    
+    nodes_dict = dict()
+    current_key = None
+    current_time = None
+    current_dependence = list()
+    current_reading = False
+    current_node_number = 0
+    first_node_key = inf
+
+    with open(doc, 'r') as f:
+        parser = ijson.parse(f)
+        for prefix, event, value in parser:
+            # Start reading a node :
+            if event=='map_key' and prefix=='nodes':
+                current_key = value
+                current_reading = True
+                current_node_number += 1
+            if current_reading and current_key!=None:
+                if prefix=='nodes.'+current_key+'.Data' and event=='string':
+                    current_time = time_personalized.getTimeFromData(value)
+                elif prefix=='nodes.'+current_key+'.Dependencies.item' and event=='number':
+                    if deleteDependencies:
+                        if value>first_node_key:
+                            current_dependence.append(value)
+                    else:
+                        current_dependence.append(value)
+                elif prefix=='nodes.'+current_key and event=='end_map':                        
+                    if current_node_number>nodeStart:
+                        nodes_dict[int(current_key)] = task.Task(int(current_key), current_time, current_dependence)
+                        if current_node_number==nodeStart+1:
+                            first_node_key=int(current_key)
+                    if current_node_number==nodeEnd:
+                        return nodes_dict
+                    current_key = None
+                    current_time = None
+                    current_dependence = list()
+                    current_reading = False
+    return nodes_dict
 
 
 def ideal_time(dict):
